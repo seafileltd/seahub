a8777c2dc189ca976e014e2d063755326cb012c2
zhengxie <xiez1989@gmail.com>
2018-07-06 17:55:17 +0800

    wip: Rewrite file history page using ReactJS

---
+++ frontend/src/components/FileHistory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { gettext as _ } from '../globals';
+
+export class FileHistory extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h2>{_('Test React trans')}</h2>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/globals.js
@@ -0,0 +1,1 @@
+export const gettext = window.gettext;
