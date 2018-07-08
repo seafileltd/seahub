ec903141c37236ef71429213ebd4ef96d70ae19b
zhengxie <xiez1989@gmail.com>
2018-07-08 23:35:24 +0800

    refactor

--- frontend/src/components/FileHistory.js
+++ frontend/src/components/FileHistory.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { gettext as _, siteRoot, lang, getUrl } from '../globals';
 import Moment from 'react-moment';
 import filesize from 'filesize';
-import { compose } from 'recompose';
 import PropTypes from 'prop-types';
 
 const PER_PAGE = 25;
@@ -24,7 +23,7 @@ const applyUpdateResult = (result) => (prevState) => ({
 });
 
 const getFileHistoryUrl = (repo_id, path, page=1, per_page=PER_PAGE) => {
-  return `${siteRoot}api/v2.1/repos/${repo_id}/file/new_history/?path=${path}&page=${page}&per_page=${per_page}`;
+  return `${siteRoot}api/v2.1/repos/${repo_id}/file/new_history/?path=${encodeURIComponent(path)}&page=${page}&per_page=${per_page}`;
 };
 
 class FileHistory extends React.Component {
@@ -59,7 +58,7 @@ class FileHistory extends React.Component {
   }
 
   onHandleErrors = (error) => {
-    alert(error);
+    alert(error);               // TODO: better error message
     return;
   }
 
@@ -77,23 +76,32 @@ class FileHistory extends React.Component {
         <BackNav />
         <Tip />
 
-        <Breadcrumb repoID={REPO_ID}
-                    repoName={window.app.pageOptions.repoName}
-                    filePath={FILE_PATH} />
+        <Breadcrumb
+          repoID={REPO_ID}
+          repoName={window.app.pageOptions.repoName}
+          filePath={FILE_PATH} />
 
-        <TableWithLoadMore
+        <Table
           data={this.state.data}
           onPaginatedGet={this.onPaginatedGet}
           isLoading={this.state.isLoading}
           hasMore={this.state.hasMore}
-          page={this.state.page}
-        />
+          page={this.state.page} />
+
+        {
+          (this.state.page !== null && this.state.hasMore) &&
+            <LoadMoreIndicator
+              onPaginatedGet={this.onPaginatedGet}
+              isLoading={this.state.isLoading}
+            />
+        }
 
       </div>
     );
   }
 }
 
+// presentational components
 const Header = () => (
   <h2><span className="op-target">{window.app.pageOptions.fileName}</span> Version History</h2>
 );
@@ -108,47 +116,24 @@ const Tip = () => (
   <p className="tip">Tip: a new version will be generated after each modification, and you can restore the file to a previous version.</p>
 );
 
-class TableRow extends React.Component {
-  constructor(props) {
-    super(props);
+const TableRow = ({ item, idx, onMouseLeave=f=>f, onMouseEnter=f=>f, isHovered=false }) => (
+  <tr onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}
+    className={isHovered ? 'hl' : ''}>
+    <td className="time"><span title={item.ctime}><Moment locale={lang} fromNow>{item.ctime}</Moment></span> {idx === 0 && '(current version)'}</td>
+    <td><img src={item.creator_avatar_url} width="16" height="16" alt="" className="avatar" /> <a href={getUrl({name: 'user_profile', username: item.creator_email})} className="vam">{item.creator_name}</a></td>
+    <td>{filesize(item.size, {base: 10})}</td>
 
-    this.state = {
-      isSelected: false,
-    };
-  }
-
-  mouseEnter = () => {
-    this.setState( {isSelected: true} );
-  }
-
-  mouseLeave = () => {
-    this.setState( {isSelected: false} );
-  }
-
-  render() {
-    const { item, idx } = this.props;
-    const { isSelected } = this.state;
-    return (
-      <tr onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}
-        className={isSelected ? 'hl' : ''}>
-        <td className="time"><span title={item.ctime}><Moment locale={lang} fromNow>{item.ctime}</Moment></span> {idx === 0 && '(current version)'}</td>
-        <td><img src={item.creator_avatar_url} width="16" height="16" alt="" className="avatar" /> <a href={getUrl({name: 'user_profile', username: item.creator_email})} className="vam">{item.creator_name}</a></td>
-        <td>{filesize(item.size, {base: 10})}</td>
-
-        { isSelected ?
-          <td>
-            {idx !== 0 && <a href="#" className="op" target="_blank">Restore</a>}
-            <a href={getUrl({name: 'download_historic_file', repoID: REPO_ID, objID: item.rev_file_id, filePath: FILE_PATH})} className="op" target="_blank">Download</a>
-            <a href={getUrl({name: 'view_historic_file', repoID: REPO_ID, commitID: item.commit_id, objID: item.rev_file_id, filePath: FILE_PATH})} className="op" target="_blank">View</a>
-            <a href={getUrl({name: 'diff_historic_file', repoID: REPO_ID, commitID: item.commit_id, filePath: FILE_PATH})} className="op" target="_blank">Diff</a>
-          </td> :
-          <td></td>
-        }
-      </tr>
-    );
-  }
-}
-
+    { isHovered ?
+      <td>
+        {idx !== 0 && <a href="#" className="op" target="_blank">Restore</a>}
+        <a href={getUrl({name: 'download_historic_file', repoID: REPO_ID, objID: item.rev_file_id, filePath: FILE_PATH})} className="op" target="_blank">Download</a>
+        <a href={getUrl({name: 'view_historic_file', repoID: REPO_ID, commitID: item.commit_id, objID: item.rev_file_id, filePath: FILE_PATH})} className="op" target="_blank">View</a>
+        <a href={getUrl({name: 'diff_historic_file', repoID: REPO_ID, commitID: item.commit_id, filePath: FILE_PATH})} className="op" target="_blank">Diff</a>
+      </td> :
+      <td></td>
+    }
+  </tr>
+);
 TableRow.propTypes = {
   idx: PropTypes.number.isRequired,
   item: PropTypes.shape({
@@ -174,7 +159,7 @@ const Table = ({ data }) => (
 
     <tbody>
       {data.map(
-        (item, idx) => <TableRow key={item.commit_id} item={item} idx={idx} />
+        (item, idx) => <TableRowWithHover key={item.commit_id} item={item} idx={idx} />
       )}
     </tbody>
   </table>
@@ -213,28 +198,46 @@ Breadcrumb.propTypes = {
   filePath: PropTypes.string.isRequired,
 };
 
-const withLoadMore = (conditionFn) => (Component) => (props) =>
-  <div>
-    <Component {...props} />
-
-    { conditionFn(props) &&
-    <div id="history-more">
-      { props.isLoading &&
-      <div id="history-more-loading">
-        <span className="loading-icon loading-tip"></span>
-      </div>
-      }
-
-      <button id="history-more-btn" onClick={props.onPaginatedGet} className="full-width-btn">{_('More')}</button>
+const LoadMoreIndicator = ({ isLoading, onPaginatedGet }) => (
+  <div id="history-more">
+    { isLoading &&
+    <div id="history-more-loading">
+      <span className="loading-icon loading-tip"></span>
     </div>
     }
-  </div>;
 
-const loadMoreCondition = props =>
-  props.page !== null && props.hasMore;
+    <button id="history-more-btn" onClick={onPaginatedGet} className="full-width-btn">{_('More')}</button>
+  </div>
+);
 
-const TableWithLoadMore = compose(
-  withLoadMore(loadMoreCondition),
-)(Table);
+
+// HoC
+const withHover = (Component) => (
+  class WithHover extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isHovered: false
+      };
+    }
+
+    onMouseEnter = () => {
+      this.setState({ isHovered: true });
+    }
+
+    onMouseLeave = () => {
+      this.setState({ isHovered: false });
+    }    
+
+    render() {
+      return <Component {...this.state} {...this.props}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      />;
+    }
+  }
+);
+
+const TableRowWithHover = withHover(TableRow);
 
 export default FileHistory;
